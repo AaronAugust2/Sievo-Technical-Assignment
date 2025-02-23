78b37449c77e451a5d8c11db209b39ec36727c61
Aaron Äärelä <aaron.aarela@aalto.fi>
2025-02-23 23:28:47 +0200

    Add IO error handling

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -32,30 +32,40 @@ namespace application {
 
             return hiker;   
         }
+        
         static void Main(string[] args) {
             Console.Write("\n***Welcome to the application! We shall begin with entering details for hiker 1 & 2:***\n");
+            
+            // I had to add exception handling here because an exception due to user error
+            // would bug out the terminal due to some strange control character sequence or something. 
+            // Not the most polished solution but this was outside of the definition of the assignment already. 
+            // Just a nice quality of life when testing out the application, in the case of user error :)
+            try {
+                Hiker hiker1 = CreateHikerFromIO(1);
+                Hiker hiker2 = CreateHikerFromIO(2);
 
-            Hiker hiker1 = CreateHikerFromIO(1);
-            Hiker hiker2 = CreateHikerFromIO(2);
-
-            Console.Write("\n***The following data was inputted:***\n");
-            Console.Write("\nHiker 1:\n");
-            Console.Write(hiker1.Stringify());
-            Console.Write("\n\nHiker 2:\n");            
-            Console.Write(hiker2.Stringify());
-            Console.WriteLine("");
-
-            bool successOfTrade = hiker1.TryTrade(hiker2);
-
-            if(successOfTrade) {
-                Console.Write("\n***Trade between Hiker 1 & Hiker 2: Trade completed, see updated info below:***");
+                Console.Write("\n***The following data was inputted:***\n");
                 Console.Write("\nHiker 1:\n");
                 Console.Write(hiker1.Stringify());
-                Console.Write("\nHiker 2:\n");            
-                Console.Write(hiker2.Stringify() + "\n");
-            } else {
-                Console.Write("\n***Trade between Hiker 1 & Hiker 2: Trade unsuccessful, see explanation below:***");
-                Console.Write(hiker1.TradeFailureReason(hiker2) + "\n");
+                Console.Write("\n\nHiker 2:\n");            
+                Console.Write(hiker2.Stringify());
+                Console.WriteLine("");
+
+                bool successOfTrade = hiker1.TryTrade(hiker2);
+
+                if(successOfTrade) {
+                    Console.Write("\n***Trade between Hiker 1 & Hiker 2: Trade completed, see updated info below:***\n");
+                    Console.Write("\nHiker 1:\n");
+                    Console.Write(hiker1.Stringify());
+                    Console.Write("\nHiker 2:\n");            
+                    Console.Write(hiker2.Stringify() + "\n");
+                } else {
+                    Console.Write("\n***Trade between Hiker 1 & Hiker 2: Trade unsuccessful, see explanation below:***");
+                    Console.Write(hiker1.TradeFailureReason(hiker2) + "\n");
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Invalid IO input, exiting program.");
+                Environment.Exit(1);
             }
         }   
     }
